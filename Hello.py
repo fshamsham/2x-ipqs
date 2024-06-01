faeb903bee570a034981fcc2f9fc4c0d4d6b0076
Fang Ying <105067396+fshamsham@users.noreply.github.com>
2024-06-01 22:19:03 +0800

    first commit

---
+++ Hello.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
+
+st.write("# This is a trial version! 👋")
+
+st.sidebar.success("Select a tool above.")
+
---
+++ README.md
@@ -0,0 +1,1 @@
+# list-upload-data-check
